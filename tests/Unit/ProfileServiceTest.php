b49fbcb30394c4276e8edaba99adfcb365dbbf0d
Giorbis Miguel <giorbismiguel@gmail.com>
2023-02-05 09:51:55 -0500

    Implement test for unit algorithmic problem resolved

--- tests/Unit/ProfileServiceTest.php
+++ tests/Unit/ProfileServiceTest.php
@@ -2,17 +2,63 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Profile;
+use App\Services\ProfileFriendsService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ProfileServiceTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_shorter_connection_between_friends()
     {
-        $this->assertTrue(true);
+        $robert = Profile::factory()->create([
+            'first_name' => 'Roberto',
+            'last_name'  => 'Perez'
+        ]);
+
+        $ana = Profile::factory()->create([
+            'first_name' => 'Ana',
+            'last_name'  => 'Suarez'
+        ]);
+
+        $juan = Profile::factory()->create([
+            'first_name' => 'Juan',
+            'last_name'  => 'Perez'
+        ]);
+
+        $maykel = Profile::factory()->create([
+            'first_name' => 'Maykel',
+            'last_name'  => 'Perez'
+        ]);
+
+        $leo = Profile::factory()->create([
+            'first_name' => 'Leo',
+            'last_name'  => 'Perez'
+        ]);
+
+        $robert->friends = [$ana->id, $juan->id];
+        $robert->save();
+
+        $juan->friends = [$maykel->id];
+        $juan->save();
+
+        $maykel->friends = [$leo->id];
+        $maykel->save();
+
+        $ana->friends = [$leo->id];
+        $ana->save();
+        
+        $connections = (new ProfileFriendsService())->getConnections($robert, $leo, []);
+
+        $this->assertIsArray($connections);
+
+        $this->assertEquals($connections[0], "{$ana->first_name} - {$ana->id}");
     }
 }
